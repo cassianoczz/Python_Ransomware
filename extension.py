516d5083b701370e662faf1c3c375f66f6df371d
cassianoczz <cassianoczz@users.noreply.github.com>
2022-10-29 10:32:11 -0300

    Arquivos com todas as extensoes que o Ransom criptografa

---
+++ extension.py
@@ -0,0 +1,17 @@
+extension = [
+    #'exe', 'dll', 'so', 'rpm', 'deb', 'vmlinuz', 'img', #Arquivos Sistema
+    'jpg', 'jpeg', 'bmp', 'gif', 'png', 'svg', 'psd', 'raw', #Imagens
+    'mp3', 'mp4', 'm4a', 'aac', 'ogg', 'flac', 'wav', 'wma', 'aiff', 'ape', #Audios
+    'avi', 'flv', 'm4v', 'mkv', 'mov', 'mpg', 'mpeg', 'wmv', 'swf', '3gp', #Videos
+    'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', #Office
+    'odt', 'odp', 'ods', 'txt', 'rtf', 'tex', 'pdf', 'epub', 'md', #Open Office
+    'yml', 'yaml', 'json', 'xml', 'csv', #dados e Config
+    'db', 'sql', 'dbf', 'mdb', 'iso', #banco de dados
+    'html', 'htm', 'xhtml', 'php', 'asp', 'aspx', 'js', 'jsp', 'css', #web
+    'c', 'cpp', 'cxx', 'h', 'hpp', 'hxx', #C
+    'java', 'class', 'jar' #java
+    'ps', 'bat', 'vb', #Windows
+    'awk', 'sh', 'cgi', 'pl', 'ada', 'swift'#unix
+    'go', 'py', 'pyc', 'bf', 'coffee', #fontes
+    'zip', 'tar', 'tgz', 'bz2', '7z', 'rar', 'bak', #Compact Bkp 
+]
