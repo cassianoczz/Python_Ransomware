15fac7cf5746e64259a96b826941c7e041a22ec4
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-07 07:02:15 -0300

    Alterado nome do arquivo para melhor compreensão

---
+++ PathFolderArchives.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import extension
+
+import os
+
+
+INITIAL_PATH = os.path.abspath(os.path.join(os.getcwd(), 'Archives'))
+
+
+def discover(path):
+    for dirpath, dirs, files in os.walk(path):
+        for _file in files:
+            absoluts_path = os.path.abspath(os.path.join(dirpath, _file))
+            ext = absoluts_path.split('.')[-1]
+            if ext in extension.extension:
+                yield absoluts_path
